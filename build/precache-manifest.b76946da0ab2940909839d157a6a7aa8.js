cc97d54560270f32c4f6ef3ff4d91bc86503e273
Alex Cameron <alexrcameron@gmail.com>
2020-09-23 16:19:18 -0500

    Initial commit

---
+++ build/precache-manifest.b76946da0ab2940909839d157a6a7aa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcdd022527a7039710013fe6b18518ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1db928ca97508328b57",
+    "url": "/static/css/main.266173d9.chunk.css"
+  },
+  {
+    "revision": "18ef7640e4ac4cae40c0",
+    "url": "/static/js/2.84082156.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.84082156.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1db928ca97508328b57",
+    "url": "/static/js/main.4f7ce213.chunk.js"
+  },
+  {
+    "revision": "a4e456a797559e1ba170",
+    "url": "/static/js/runtime-main.c4673e62.js"
+  }
+]);
